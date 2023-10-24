9d5af3f8de282820818f0c20557e64cf892b25a9
Joes Lie <liej6799@protonmail.ch>
2023-10-24 13:09:30 +0700

    fix sharpness, add bar for comparison.

--- web/script.js
+++ web/script.js
@@ -16,9 +16,9 @@ uniform float width;
 uniform float height;
 uniform float texWidth;
 uniform float texHeight;
-const float sharpness = 0.4;
+const float sharpness = 1.3;
 uniform sampler2D iChannel0;
-
+float width_half;
 
 vec4 getPixel(vec2 pos) {
 	vec2 coord = (pos + .5) / vec2(width, height);
@@ -28,49 +28,58 @@ vec4 getPixel(vec2 pos) {
 
 
 void main() {
-
-  vec3 a = getPixel(gl_FragCoord.xy + vec2(-1.0,-1.0)).rgb;
-  vec3 b = getPixel(gl_FragCoord.xy + vec2( 0.0,-1.0)).rgb;
-  vec3 c = getPixel(gl_FragCoord.xy + vec2( 1.0,-1.0)).rgb;
-  vec3 d = getPixel(gl_FragCoord.xy + vec2(-1.0, 0.0)).rgb;
+	width_half = width / 2.0;
   vec3 e = getPixel(gl_FragCoord.xy).rgb;
-  vec3 f = getPixel(gl_FragCoord.xy + vec2( 1.0, 0.0)).rgb;
-  vec3 g = getPixel(gl_FragCoord.xy + vec2(-1.0, 1.0)).rgb;
-  vec3 h = getPixel(gl_FragCoord.xy + vec2( 0.0, 1.0)).rgb;
-  vec3 i = getPixel(gl_FragCoord.xy + vec2( 1.0, 1.0)).rgb;;
+  if (gl_FragCoord.x > width_half){
 
-  // Soft min and max.
-  //  a b c             b
-  //  d e f * 0.5  +  d e f * 0.5
-  //  g h i             h
-  // These are 2.0x bigger (factored out the extra multiply).
+    vec3 a = getPixel(gl_FragCoord.xy + vec2(-1.0,-1.0)).rgb;
+    vec3 b = getPixel(gl_FragCoord.xy + vec2( 0.0,-1.0)).rgb;
+    vec3 c = getPixel(gl_FragCoord.xy + vec2( 1.0,-1.0)).rgb;
+    vec3 d = getPixel(gl_FragCoord.xy + vec2(-1.0, 0.0)).rgb;
 
-  vec3 mnRGB  = min(min(min(d,e),min(f,b)),h);
-  vec3 mnRGB2 = min(min(min(mnRGB,a),min(g,c)),i);
-  mnRGB += mnRGB2;
+    vec3 f = getPixel(gl_FragCoord.xy + vec2( 1.0, 0.0)).rgb;
+    vec3 g = getPixel(gl_FragCoord.xy + vec2(-1.0, 1.0)).rgb;
+    vec3 h = getPixel(gl_FragCoord.xy + vec2( 0.0, 1.0)).rgb;
+    vec3 i = getPixel(gl_FragCoord.xy + vec2( 1.0, 1.0)).rgb;;
 
-  vec3 mxRGB  = max(max(max(d,e),max(f,b)),h);
-  vec3 mxRGB2 = max(max(max(mxRGB,a),max(g,c)),i);
-  mxRGB += mxRGB2;
+    // Soft min and max.
+    //  a b c             b
+    //  d e f * 0.5  +  d e f * 0.5
+    //  g h i             h
+    // These are 2.0x bigger (factored out the extra multiply).
 
-  // Smooth minimum distance to signal limit divided by smooth max.
+    vec3 mnRGB  = min(min(min(d,e),min(f,b)),h);
+    vec3 mnRGB2 = min(min(min(mnRGB,a),min(g,c)),i);
+    mnRGB += mnRGB2;
 
-  vec3 rcpMxRGB = vec3(1)/mxRGB;
-  vec3 ampRGB = clamp((min(mnRGB,2.0-mxRGB) * rcpMxRGB),0.0,1.0);
-  // Shaping amount of sharpening.
-  ampRGB = inversesqrt(ampRGB);
-  float peak = 8.0 - 3.0 * sharpness;
-  vec3 wRGB = -vec3(1)/(ampRGB * peak);
-  vec3 rcpWeightRGB = vec3(1)/(1.0 + 4.0 * wRGB);
-  //                          0 w 0
-  //  Filter shape:           w 1 w
-  //                          0 w 0  
+    vec3 mxRGB  = max(max(max(d,e),max(f,b)),h);
+    vec3 mxRGB2 = max(max(max(mxRGB,a),max(g,c)),i);
+    mxRGB += mxRGB2;
 
-  vec3 window = (b + d) + (f + h);
-  vec3 outColor = clamp((window * wRGB + e) * rcpWeightRGB,0.0,1.0);
+    // Smooth minimum distance to signal limit divided by smooth max.
 
-  gl_FragColor = vec4(outColor,getPixel(gl_FragCoord.xy).a);
-  //gl_FragColor = vec4(e,getPixel(gl_FragCoord.xy).a);
+    vec3 rcpMxRGB = vec3(1)/mxRGB;
+    vec3 ampRGB = clamp((min(mnRGB,2.0-mxRGB) * rcpMxRGB),0.0,1.0);
+
+    // Shaping amount of sharpening.
+    ampRGB = inversesqrt(ampRGB);
+    float peak = 8.0 - 3.0 * sharpness;
+    vec3 wRGB = -vec3(1)/(ampRGB * peak);
+    vec3 rcpWeightRGB = vec3(1)/(1.0 + 4.0 * wRGB);
+    //                          0 w 0
+    //  Filter shape:           w 1 w
+    //                          0 w 0  
+
+    vec3 window = (b + d) + (f + h);
+    vec3 outColor = clamp((window * wRGB + e) * rcpWeightRGB,0.0,1.0);
+
+    gl_FragColor = vec4(outColor,getPixel(gl_FragCoord.xy).a);
+  }
+  else {
+    gl_FragColor = vec4(e,getPixel(gl_FragCoord.xy).a);
+  }
+  if (gl_FragCoord.x > width_half - 1.0 && gl_FragCoord.x < width_half + 1.0) gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+  
 }
 `;
 
@@ -82,8 +91,7 @@ function loop(time) {
 
   glea.setActiveTexture(0, camTexture);
   gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, gl.RGBA, gl.UNSIGNED_BYTE, video);  
-  glea.uni('texWidth', video.videoWidth);
-  glea.uni('texHeight', video.videoHeight);
+
 
   glea.clear();
   glea.uni('width', glea.width);
@@ -124,7 +132,7 @@ async function setup() {
     glea.setActiveTexture(0, camTexture);
     loop(0);
   }, true);
-  video.play();
+
 }
 
 setup();
